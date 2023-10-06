f515045131047a0b9db52496fd33cd7d53550a6f
moman13 <35956421+moman13@users.noreply.github.com>
2023-10-07 01:26:05 +0300

    just clean up

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth;
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Million',
+        ),
+      ),
+      body: Container(
+        child: Column(children: []),
+      ),
+    );
+  }
+}
